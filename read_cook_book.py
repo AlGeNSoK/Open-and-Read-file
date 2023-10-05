26508474d0309ee5c2848d93b19921790e74b881
Aleksandr Sokolov <algensok@gmail.com>
2023-10-06 00:22:28 +0300

    Task 2. Add function get_shop_list_by_dishes(dishes, person_count)

--- read_cook_book.py
+++ read_cook_book.py
@@ -10,15 +10,29 @@ def read_file(read_file):
                 if id_ < 2:
                     continue
                 ingredient_list = meal_name[id_].split(" | ")
-                ingredient_name = ingredient_list[0]
-                quantity = ingredient_list[1]
-                measure = ingredient_list[2]
-                ingredients_list.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+                ingredients_list.append({'ingredient_name': ingredient_list[0], 'quantity': ingredient_list[1], 'measure': ingredient_list[2]})
 
             cook_book[dish_name] = ingredients_list
 
     return cook_book
 
 
-cook_book = read_file('recipes.txt')
-print(cook_book)
+def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = read_file('recipes.txt')
+    ingredients_list = {}
+    for dish, ingredients in cook_book.items():
+        if dish in dishes:
+            for ingredient in ingredients:
+                if ingredient['ingredient_name'] in ingredients_list.keys():
+                    ingredients_list[ingredient['ingredient_name']] = {
+                        'quantity': ingredients_list[ingredient['ingredient_name']]['quantity'] +
+                                    int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
+                else:
+                    ingredients_list[ingredient['ingredient_name']] = {
+                        'quantity': int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
+
+    return ingredients_list
+
+
+shop_list_by_dishes = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(shop_list_by_dishes)
